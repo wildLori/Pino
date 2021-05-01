f4e69fa6d8dfe2d3da56563db70b53e6681a9b0a
wildLori <loribelfa02@gmail.com>
2021-05-01 12:39:43 +0200

    Web Client

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+
 const modelManager = require('../helper/modelManager')
 const path = require('path');
+const cookieParser = require('cookie-parser')
 
 require('dotenv').config({
     path: path.resolve(__dirname, '../private/.env')
@@ -12,7 +14,17 @@ const privateKey = process.env.JWT;
 /**
  * 🍎 ROUTING
  */
+router.use(cookieParser());
 
+// router.use(function (req, res, next) {
+//     console.log("Guardo i jwt");
+//     var login_cookie = req.cookies.jwt;
+//     if (login_cookie === undefined) {
+
+//     }
+//     console.log("Vabbe vado avanti")
+//     next();
+// })
 router.get('/', (req, res) => {
     if (req)
         res.render('login', {
@@ -25,35 +37,39 @@ router.post('/login', async (req, res) => {
     console.log("Si ok fra mi è arrivato: " + req.body.username, req.body.pin)
     const username = req.body.username;
     const pin = req.body.pin;
-    if (username) {
-        if (pin) {
-            //TODO: Questa funzione deve ritornare : JSON { username, ruolo }
-            await modelManager.apriDB();
-            const user = await modelManager.getUserAndRole(username, pin);
-            modelManager.chiudiDB();
+    if (username != "" && pin != "") {
+        //TODO: Questa funzione deve ritornare : JSON { username, ruolo }
+        await modelManager.apriDB();
+        const user = await modelManager.getUserAndRole(username, pin);
+        await modelManager.chiudiDB();
 
-            if (user) {
-                // generate an access token
-                const accessToken = jwt.sign({
-                    nome: user.nome,
-                    admin: user.is_admin
-                }, privateKey);
+        if (user) {
+            console.log("Si fra c'è lo user");
+            // generate an access token
+            const accessToken = jwt.sign({
+                nome: user.nome,
+                admin: user.is_admin
+            }, privateKey);
 
+            // res.cookie("jwt", accessToken, {
+            //     maxAge: 90000, // Lifetime
+            // })
 
-                if (user.is_admin) {
-                    res.json({
-                        accessToken
-                    });
-                } else {
-                    res.json("Unauthorized");
-                }
-            }
+            res.cookie('jwt', accessToken, {
+                httpOnly: true
+            });
+            res.redirect(302, 'https://belfanti.ddns.net:8443/officina') // Front-End App
+            // res.json({
+            //     accessToken
+            // });
         } else {
-            return res.send("Erorre con il pin");
+            res.json({
+                error: "Error"
+            });
         }
-    } else {
-        return res.send("Errore con lo Username")
-    }
+    } else(res.json({
+        error: "Error"
+    }))
 });
 
 
--- routes/officina.js
+++ routes/officina.js
@@ -1,29 +1,35 @@
-const {
-    Router
-} = require('express')
+const express = require('express')
 const path = require('path');
-const router = Router()
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const {
+    decode
+} = require('punycode');
 require('dotenv').config({
     path: path.resolve(__dirname, '../private/.env')
 })
 const privateKey = process.env.JWT;
 
+router.use(cookieParser());
 const authenticateJWT = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-
-    if (authHeader) {
-        const token = authHeader.split(' ')[1];
-
-        jwt.verify(token, privateKey, (err, user) => {
+    console.log("Entro in officina");
+    // const authHeader = req.headers.authorization;
+    var login_cookie = req.cookies.jwt;
+    if (login_cookie === undefined) {
+        res.redirect("/auth");
+        //res.send("No fra, no non hai il cookie");
+    } else {
+        console.log(login_cookie);
+        jwt.verify(login_cookie, privateKey, (err, user) => {
             if (err) {
                 return res.sendStatus(403);
             }
             console.log("Utente " + user + "jwttato");
             req.user = user;
             next();
-        });
-    } else {
-        return res.sendStatus(401);
+        })
+
     }
 }
 
@@ -31,10 +37,10 @@ const authenticateJWT = (req, res, next) => {
  * 🔨 ROUTING
  */
 router.get('/', authenticateJWT, function (req, res) {
-    if (req)
-        res.render('officina', {
-            'title': 'Application Home'
-        })
+    if (req) {
+        console.log("SKU, ora ti renderizzo la officina");
+        res.render('officina')
+    }
 
 })
 
